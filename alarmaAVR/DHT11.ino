b40bd96e8474ddf3e1d648a99960fd2dd057458a
Nicolás Putallaz <77561348+putallazNicolas@users.noreply.github.com>
2024-10-31 17:17:10 -0300

    Add files via upload

--- alarmaAVR/DHT11.ino
+++ alarmaAVR/DHT11.ino
@@ -1,58 +1,104 @@
 #define DHT11 PD2
 
-uint8_t *DHT_DDR = &DDRD;
-uint8_t *DHT_PORT = &PORTD;
-uint8_t *DHT_PIN = &PIND;
+/*
+//COPIAR EN MAIN
+typedef struct
+{
+  int humedad_ent;
+  int humedad_dec;
+  int temperatura_ent;
+  int temperatura_dec;
+  int checksum;
+}
+dhtData;
 
+dhtData dht11 = {0, 0, 0, 0, 0};
+*/
+
+
+
+
+/*
 int DHT(void) // Retorna 0 si hubo un error, retorna 1 si fue realizado correctamente
 {
-  int humedad_ent = 0;
-  int humedad_dec = 0;
-  int temperatura_ent = 0;
-  int temperatura_dec = 0;
-  int checksum = 0;
-
   // Empieza la comunicación
-  *DHT_DDR |= (1 << DHT11); 
-  *DHT_PORT &= ~(1 << dht11);
+  DDRD |= (1 << DHT11); 
+  PORTD &= ~(1 << DHT11);
   _delay_ms(18);
-  *DHT_PORT |= (1 << DHT11);
-  _delay_us(30);
+  PORTD |= (1 << DHT11);
+  DDRD &= ~(1 << DHT11);
+  _delay_us(40);
 
   // El DHT11 chequea que se recibieron bien los datos
-  *DHT_DDR &= ~(1 << DHT11);
-  if (DHT_PIN & (1 << DHT11) == 1) return 0;
+  if (PIND & (1 << DHT11))
+  {
+    //Serial.println("Checkpoint 1");
+    _delay_ms(20);
+    return 0;
+  }
   _delay_us(80);
-  if (DHT_PIN & (1 << DHT11) == 0) return 0;
-  _delay_us(80);
+  if (!(PIND & (1 << DHT11)))
+  {
+    //Serial.println("Checkpoint 2");
+    _delay_ms(20);
+    return 0;
+  }
 
   // El DHT11 empieza a enviar datos, empezando por el entero de la humedad
-  humedad_ent = leerDHT();
-  humedad_dec = leerDHT();
-  temperatura_ent = leerDHT();
-  temperatura_dec = leerDHT();
-  checksum = leerDHT();
-  // Falta chequear la sum
+  dht11.humedad_ent = leerDHT();
+  dht11.humedad_dec = leerDHT();
+  dht11.temperatura_ent = leerDHT();
+  dht11.temperatura_dec = leerDHT();
+  dht11.checksum = leerDHT();
+  
+  if (dht11.checksum == dht11.humedad_ent + dht11.humedad_dec + dht11.temperatura_ent + dht11.temperatura_dec)
+  {
+    return 1;
+  }
+  else
+  {
+    //Serial.println("Checkpoint 3");
+    _delay_ms(20);
+    return 0;
+  }
+  return 1;
 }
 
 int leerDHT(void)
 {
   int valor = 0;
+  //while (PIND & (1 << DHT11));
   for (int i = 0; i < 8; i++)
   {
-    if (DHT_PIN & (1 << DHT11) == 1) return 0;
+    if (PIND & (1 << DHT11)) 
+    {
+      //Serial.println("Checkpoint 4");
+      _delay_ms(20);
+      return 0;
+    }
+    
     _delay_us(50);
-    if (DHT_PIN & (1 << DHT11) == 0) return 0;
+    
+    if (!(PIND & (1 << DHT11)))
+    {
+      //Serial.println("Checkpoint 5");
+      _delay_ms(20);
+      return 0;
+    }
+    
     _delay_us(28);
-    if (DHT_PIN & (1 << DHT11) == 0)
+    
+    if (!(PIND & (1 << DHT11)))
     {
       valor &= ~(1 << (7 - i));
     }
     else
     {
       valor |= (1 << (7 - i));
-      while (DHT_PIN & (1 << DHT11) == 0); // Espera a que baje
+      while (!(PIND & (1 << DHT11))); // Espera a que baje
     }
   }
   return valor;
 }
+
+*/
--- alarmaAVR/alarmaAVR.ino
+++ alarmaAVR/alarmaAVR.ino
@@ -1,7 +1,6 @@
 int main(void)
 {
   Serial.begin(9600);
-  tiempoDeclare();
   while(1)
   {
     
