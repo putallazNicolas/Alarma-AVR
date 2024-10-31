aa8894b30430b3b99a2e9b1ebc5fb20165fc604e
MatuTorres <dados2d@gmail.com>
2024-10-31 17:20:33 -0300

    Update alarmaAVR.ino para tiempo y escribir con keypad
    poner las funciones para tiempo y el keypad en main

--- alarmaAVR/alarmaAVR.ino
+++ alarmaAVR/alarmaAVR.ino
@@ -1,8 +1,14 @@
 int main(void)
 {
   Serial.begin(9600);
+  tiempoDeclare();
+  setupKeypad();
   while(1)
   {
-    
+    char key = checkKeypad();
+    if(key)
+    {
+      Serial.println(key);
+    }
   }
 }
