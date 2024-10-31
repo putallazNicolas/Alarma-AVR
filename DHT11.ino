3482a1820a52ff8d411ba72ad2c681d7e064ecf8
Nicolás Putallaz <77561348+putallazNicolas@users.noreply.github.com>
2024-10-31 16:08:56 -0300

    Add files via upload

---
+++ DHT11.ino
@@ -0,0 +1,80 @@
+#define DHT11 PD2
+
+int DHT(void) // Retorna 0 si hubo un error, retorna 1 si fue realizado correctamente
+{
+  // Empieza la comunicación
+  DDRD |= (1 << DHT11); 
+  PORTD &= ~(1 << DHT11);
+  _delay_ms(18);
+  PORTD |= (1 << DHT11);
+  DDRD &= ~(1 << DHT11);
+  _delay_us(30);
+  //while(PIND & (1 << DHT11));
+
+  // El DHT11 chequea que se recibieron bien los datos
+  if (PIND & (1 << DHT11))
+  {
+    Serial.println("Checkpoint 1");
+    _delay_ms(20);
+    return 0;
+  }
+  _delay_us(80);
+  if (!(PIND & (1 << DHT11)))
+  {
+    //Serial.println("Checkpoint 2");
+    _delay_ms(20);
+    return 0;
+  }
+  _delay_us(80);
+
+  // El DHT11 empieza a enviar datos, empezando por el entero de la humedad
+  dht11.humedad_ent = leerDHT();
+  dht11.humedad_dec = leerDHT();
+  dht11.temperatura_ent = leerDHT();
+  dht11.temperatura_dec = leerDHT();
+  dht11.checksum = leerDHT();
+  
+  if (dht11.checksum == dht11.humedad_ent + dht11.humedad_dec + dht11.temperatura_ent + dht11.temperatura_dec)
+  {
+    return 1;
+  }
+  else
+  {
+    //Serial.println("Checkpoint 3");
+    _delay_ms(20);
+    return 0;
+  }
+  return 1;
+}
+
+int leerDHT(void)
+{
+  int valor = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    if (PIND & (1 << DHT11)) 
+    {
+      //Serial.println("Checkpoint 4");
+      _delay_ms(20);
+      return 0;
+    }
+    _delay_us(50);
+    if (!(PIND & (1 << DHT11)))
+    {
+      //Serial.println("Checkpoint 5");
+      _delay_ms(20);
+      return 0;
+    }
+    _delay_us(28);
+    if (!(PIND & (1 << DHT11)))
+    {
+      valor &= ~(1 << (7 - i));
+    }
+    else
+    {
+      valor |= (1 << (7 - i));
+      while (!(PIND & (1 << DHT11))); // Espera a que baje
+    }
+  }
+  return valor;
+}
