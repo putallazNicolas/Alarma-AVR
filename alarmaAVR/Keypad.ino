7eaa84efcd12fbbf9a70bab0d306993c3d803810
Mateo Torres <dados2d@gmail.com>
2024-11-07 17:20:26 -0300

    sigue sin funcionar sad

--- alarmaAVR/Keypad.ino
+++ alarmaAVR/Keypad.ino
@@ -1,60 +1,32 @@
-int flag = 0;
-
 char keys[4][4] =
 {
-{ '1','2','3', 'A' },
-{ '4','5','6', 'B' },
-{ '7','8','9', 'C' },
-{ '#','0','*', 'D' }
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '#', '0', '*', 'D' }
 };
 
 char checkKeypad()
 {
-  for (int c = 0; c < 4; c++)
+  for (int r = 0; r < 4; r++)
   {
-    PORTD &= ~(1 << c + 2);
-    
-    for(int r = 0; r < 4; r++)
+    PORTB &= ~(1 << r);
+    _delay_ms(50);
+    for (int c = 4; c < 8; c++)
     {
-      if(r < 2)
+      if ((PORTD | (1 << c)) == 0)
       {
-        if (!(PIND & (1 << (r + 6)))) 
-        {
-          while (!(PIND & (1 << (r + 6))));
-          PORTD |= (1 << c);
-          return keys[r][c];
-          flag = 0;
-        }
-      }
-      else
-      {
-        if (!(PINB & (1 << (r - 2)))) 
-        {
-          while (!(PINB & (1 << (r - 2))));
-          PORTB |= (1 << c);
-          return keys[r][c];
-          flag = 1;
-        }
+        return keys[r][c - 4];
       }
     }
-    if(flag == 0)
-  {
-    PORTD |= (1 << c);
   }
-  else
-  {
-    PORTB |= (1 << c);
-  }
-    return 0;
-  }
+  return '\0';
 }
 
 void setupKeypad()
 {
-  //columnas
-  DDRD = 0b00111100;
-  DDRB = 0b000000;
-  //filas
-  PORTD = 0b11111100;
+  DDRB = 0b001111;
+  DDRD = 0b00000000;
   PORTB = 0b111111;
+  PORTD = 0b00000000;
 }
